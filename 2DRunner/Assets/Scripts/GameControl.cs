f2d07b1a5ddf6455f302ff1d956f64dea2c9368d
mensaoliang <msl930624@gamil.com>
2016-04-15 23:01:33 -0700

    bgm

--- 2DRunner/Assets/Scripts/GameControl.cs
+++ 2DRunner/Assets/Scripts/GameControl.cs
@@ -2,15 +2,24 @@
 using System.Collections;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using UnityEngine.Audio;
 
 public class GameControl : MonoBehaviour {
     public static GameControl current;  //a reference to our game control
     public Button jumpCloudBtn;         //a reference to our jump cloud button
     public Button binaryStarBtn;        //a reference to our binary star button
     public GameObject gameOverText;
+	public AudioMixerSnapshot audio_pause;
+	public AudioMixerSnapshot audio_running;
+	public AudioMixerSnapshot audio_died;
+	public AudioClip[] stings;
+	public float bpm = 128;
 
     private bool isGameOver = false;    //is the game over?
     private bool paused = false;        //is the game paused?
+	private float m_TransitionPause;
+	private float m_TransitionRestart;
+	private float m_QuarterNote;
     
     void Awake()
     {
@@ -24,6 +33,10 @@ public class GameControl : MonoBehaviour {
 	void Start () {
         jumpCloudBtn.gameObject.SetActive(false);
         binaryStarBtn.gameObject.SetActive(false);
+		m_QuarterNote = 60 / bpm;
+		m_TransitionPause = m_QuarterNote*2;
+		m_TransitionRestart = m_QuarterNote * 4;
+
 	}
 	
 	// Update is called once per frame
@@ -32,6 +45,8 @@ public class GameControl : MonoBehaviour {
         {
             SceneManager.LoadScene(0);
             Time.timeScale = 1;
+			audio_running.TransitionTo (m_TransitionRestart);
+
         }
 	    if (Input.GetKeyDown(KeyCode.Space))//press space to pause or not pause
         {
@@ -41,6 +56,7 @@ public class GameControl : MonoBehaviour {
                 paused = false;
                 jumpCloudBtn.gameObject.SetActive(false);
                 binaryStarBtn.gameObject.SetActive(false);
+				audio_running.TransitionTo (m_TransitionRestart);
             }
             else                    //show all tool buttons here
             {
@@ -48,6 +64,7 @@ public class GameControl : MonoBehaviour {
                 paused = true;
                 jumpCloudBtn.gameObject.SetActive(true);
                 binaryStarBtn.gameObject.SetActive(true);
+				audio_pause.TransitionTo (m_TransitionPause);
             }
         }
     }
@@ -56,5 +73,7 @@ public class GameControl : MonoBehaviour {
         gameOverText.SetActive(true);
         isGameOver = true;
         Time.timeScale = 0;
+		audio_died.TransitionTo (m_TransitionPause);
+
     }
 }
